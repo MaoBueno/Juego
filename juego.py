b35c5594f9795750244d8f89d4bb6f3c4d03836d
Mauricio <mauroalternativo519@gmail.com>
2021-07-01 18:29:18 -0500

    Incluyendo bloques, error en los bloques cuando se actualiza el fondo

--- juego.py
+++ juego.py
@@ -27,7 +27,27 @@ class Jugador(pygame.sprite.Sprite):
         self.velx=0
         self.vely=0
 
+        self.bloques=pygame.sprite.Group()
+        
+
     def update(self):
+
+        self.rect.x += self.velx        # Actualizacion de la velocidad en x
+        
+
+        
+        colision=pygame.sprite.spritecollide(self, self.bloques, False)     # Control de posicion y colision con el bloque cuando el jugador se mueve en x
+        for b in colision:
+            if self.velx > 0:
+                if self.rect.right >= b.rect.left:
+                    self.rect.right = b.rect.left
+            else:
+                if self.rect.left <= b.rect.right:
+                    self.rect.left = b.rect.right
+            self.velx = 0
+    
+
+        
 
         if pygame.key.get_pressed()[pygame.K_RIGHT] or pygame.key.get_pressed()[pygame.K_LEFT] or pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_DOWN]:
             if self.columna == 0:
@@ -40,8 +60,8 @@ class Jugador(pygame.sprite.Sprite):
         
         self.image=self.imagen[self.direccion][self.columna]
 
-        # Actualizacion de la velocidad en x
-        self.rect.x += self.velx
+        
+        
 
         if self.rect.left < 0:     # Limite en x (izquierda)
             self.rect.left = 0
@@ -49,11 +69,46 @@ class Jugador(pygame.sprite.Sprite):
         
         if self.rect.right > ANCHO: # Otra forma de definir los limites
             self.rect.right = ANCHO
+        
+
+        """ self.rect.x += self.velx
+        # Control de posicion y colision con el bloque cuando el jugador se mueve en x
+        colision=pygame.sprite.spritecollide(self, self.bloques, False)
+        #print (colision)
+        for b in colision:
+            print (" For de la colision")
+            if self.velx != 0:
+                if self.rect.left < b.rect.right:
+                        self.rect.left = b.rect.right
+                        print ("< ------ ")
+                
+                if self.rect.right != b.rect.left:
+                    self.rect.right = b.rect.left
+                    print ("------ >")
+                    
+                self.velx = 0
+        
+        print ("Posicion en x: ", self.bloques) """
+        
 
 
 
         # Actualizacion de la velocidad en y
         self.rect.y += self.vely
+
+
+
+        # Control de posicion y colision con el bloque cuando el jugador se mueve en y
+        colision=pygame.sprite.spritecollide(self, self.bloques, False)
+        for b in colision:
+            if self.vely > 0:
+                if self.rect.bottom > b.rect.top:
+                    self.rect.bottom = b.rect.top
+            else:
+                if self.rect.top < b.rect.bottom:
+                    self.rect.top = b.rect.bottom
+            self.vely = 0
+
 
         # Limites en Y
         if self.rect.y < 0:
@@ -61,6 +116,52 @@ class Jugador(pygame.sprite.Sprite):
         
         if self.rect.bottom > ALTO:
             self.rect.bottom = ALTO
+        
+        
+        
+
+
+
+
+class Bloque(pygame.sprite.Sprite):
+    def __init__(self, pos, dim, cl=ROJO):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.Surface(dim)
+        self.colorb= self.image.set_alpha(50)
+        self.image.fill(cl)
+        self.rect= self.image.get_rect()
+        self.rect.x= pos[0]
+        self.rect.y= pos[1]
+        self.velx= 0
+        self.vely= 0
+        self.jugadores=pygame.sprite.Group()
+    
+    def update(self):
+        self.rect.x += self.velx
+
+        # Control de posicion y colision con el jugador cuando el bloque se mueve en x
+        colision_con_jugador=pygame.sprite.spritecollide(self, self.jugadores, False)
+        for b in colision_con_jugador:
+            if self.velx > 0:
+                if self.rect.right > b.rect.left:
+                    self.rect.right = b.rect.left
+            else:
+                if self.rect.left < b.rect.right:
+                    self.rect.left = b.rect.right
+            self.velx = 0 
+
+        self.rect.y += self.vely
+
+        # Control de posicion y colision con el jugador cuando el bloque se mueve en x
+        colision_con_jugador=pygame.sprite.spritecollide(self, self.jugadores, False)
+        for b in colision_con_jugador:
+            if self.vely > 0:
+                if self.rect.bottom > b.rect.top:
+                    self.rect.bottom = b.rect.top
+            else:
+                if self.rect.top < b.rect.bottom:
+                    self.rect.top = b.rect.bottom
+            self.vely = 0 
 
 
 # Funcion para recortar el sprite
@@ -84,7 +185,7 @@ if __name__ == '__main__':
 
 
     #Cargando el fondo
-    fondo=pygame.image.load("Fondo.png")
+    fondo=pygame.image.load("Fondo2.png")
     info=fondo.get_rect()
     f_ancho=info[2]
     f_alto=info[3]
@@ -97,11 +198,11 @@ if __name__ == '__main__':
     f_vy=0
     f_limite_y= ALTO - f_alto
 
-    lim_derecho=ANCHO-40
-    lim_izquierdo=30
+    lim_derecho=ANCHO-100
+    lim_izquierdo=100
 
-    lim_superior=40
-    lim_inferior=ALTO-60
+    lim_superior=100
+    lim_inferior=ALTO-100
 
 
     #Cargando sprites
@@ -112,10 +213,112 @@ if __name__ == '__main__':
 
     #Grupos
     jugadores = pygame.sprite.Group()
+    bloques = pygame.sprite.Group()
+
+    # Instanciacion de elementos
     j=Jugador(conejo)
+    j.bloques=bloques
     jugadores.add(j)
 
+    
+    b=Bloque([0, 0], [100, f_alto])    # limite izquierdo de la pantalla
+    b.jugadores=jugadores
+    bloques.add(b)
 
+    
+    b2=Bloque([f_ancho - 100, 0], [100, f_alto])  # limite Derecho de la pantalla
+    b2.jugadores=jugadores
+    bloques.add(b2)
+    
+    b3=Bloque([0, 0], [f_ancho, 100])    # Limite superior
+    b3.jugadores=jugadores
+    bloques.add(b3)
+
+
+    b4=Bloque([0, f_alto - 130], [f_ancho, 130])     # limite inferior
+    b4.jugadores=jugadores
+    bloques.add(b4)
+    
+
+    # Bloques parte superior del tren
+    b5=Bloque([280, 265], [40, 160]) # Sprite listo
+    b5.jugadores=jugadores
+    bloques.add(b5)
+
+    b6=Bloque([975, 590], [170, 68]) # Sprite listo
+    b6.jugadores=jugadores
+    bloques.add(b6)
+
+    b7=Bloque([1090, 990], [19, 62]) # Sprite listo
+    b7.jugadores=jugadores
+    bloques.add(b7)
+    
+    b8=Bloque([2050, 390], [19, 144]) # Sprite listo
+    b8.jugadores=jugadores
+    bloques.add(b8)
+
+    b9=Bloque([2855, 470], [646, 135]) # Sprite listo
+    b9.jugadores=jugadores
+    bloques.add(b9)
+
+    b10=Bloque([2970, 916], [22, 61]) # Sprite listo
+    b10.jugadores=jugadores
+    bloques.add(b10)
+
+    b11=Bloque([4010, 195], [21, 62]) # Sprite listo
+    b11.jugadores=jugadores
+    bloques.add(b11)
+
+    b12=Bloque([4170, 670], [21, 62]) # Sprite listo
+    b12.jugadores=jugadores
+    bloques.add(b12)
+
+    # Bloques parte inferior del tren
+    b13=Bloque([290, 1555], [20, 176]) # Sprite listo
+    b13.jugadores=jugadores
+    bloques.add(b13)
+
+    b12=Bloque([220, 2230], [644, 140]) # Sprite listo
+    b12.jugadores=jugadores
+    bloques.add(b12)
+
+    b13=Bloque([1010, 1710], [19, 65]) # Sprite listo
+    b13.jugadores=jugadores
+    bloques.add(b13)
+
+    b14=Bloque([2050, 2070], [20, 60]) # Sprite listo
+    b14.jugadores=jugadores
+    bloques.add(b14)
+    
+    b15=Bloque([1810, 1395], [174, 62]) # Sprite listo
+    b15.jugadores=jugadores
+    bloques.add(b15)
+
+    b16=Bloque([2610, 1475], [19, 66]) # Sprite listo
+    b16.jugadores=jugadores
+    bloques.add(b16)
+
+    b17=Bloque([3170, 2230], [21, 142]) # Sprite listo
+    b17.jugadores=jugadores
+    bloques.add(b17)
+
+    b18=Bloque([3690, 1910], [21, 144]) # Sprite listo
+    b18.jugadores=jugadores
+    bloques.add(b18)
+
+    b19=Bloque([4090, 1630], [175, 67]) # Sprite listo
+    b19.jugadores=jugadores
+    bloques.add(b19)
+    
+    
+    
+    
+    
+    
+    
+
+    # Variable velocidad de jugador y fondo
+    velocidad = 40
 
     reloj=pygame.time.Clock()
     fin_juego=False
@@ -127,37 +330,37 @@ if __name__ == '__main__':
                 fin = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    j.velx=15
+                    j.velx=velocidad
                     j.vely=0
                     j.direccion=0
-                    pantalla.blit(fondo, [f_x, f_y])
+                    """ pantalla.blit(fondo, [f_x, f_y])
                     pantalla.blit(conejo[j.direccion][1], [j.rect.x + 8, j.rect.y])
                     pygame.display.flip()
-                    reloj.tick(10)
+                    reloj.tick(10) """
                 if event.key == pygame.K_LEFT:
-                    j.velx=-15
+                    j.velx=-velocidad
                     j.vely=0
                     j.direccion=1
-                    pantalla.blit(fondo, [f_x, f_y])
+                    """ pantalla.blit(fondo, [f_x, f_y])
                     pantalla.blit(conejo[j.direccion][1], [j.rect.x - 8, j.rect.y])
                     pygame.display.flip()
-                    reloj.tick(10)
+                    reloj.tick(10) """
                 if event.key == pygame.K_UP:
                     j.velx=0
-                    j.vely=-15
+                    j.vely=-velocidad
                     j.direccion=2
-                    pantalla.blit(fondo, [f_x, f_y])
+                    """ pantalla.blit(fondo, [f_x, f_y])
                     pantalla.blit(conejo[j.direccion][1], [j.rect.x, j.rect.y - 10])
                     pygame.display.flip()
-                    reloj.tick(10)
+                    reloj.tick(10) """
                 if event.key == pygame.K_DOWN:
                     j.velx=0
-                    j.vely=15
+                    j.vely=velocidad
                     j.direccion=3
-                    pantalla.blit(fondo, [f_x, f_y])
+                    """ pantalla.blit(fondo, [f_x, f_y])
                     pantalla.blit(conejo[j.direccion][1], [j.rect.x, j.rect.y + 10])
                     pygame.display.flip()
-                    reloj.tick(10)
+                    reloj.tick(10) """
             if event.type == pygame.KEYUP:
                 j.vely=0
                 j.velx=0
@@ -167,11 +370,11 @@ if __name__ == '__main__':
         if j.rect.right > lim_derecho:
             j.rect.right = lim_derecho
             #j.velx=0
-            f_vx=-15
+            f_vx=-velocidad
         elif j.rect.left < lim_izquierdo:
             j.rect.left = lim_izquierdo
             #j.velx=0
-            f_vx=15
+            f_vx=velocidad
         else:
             f_vx = 0
         
@@ -180,16 +383,33 @@ if __name__ == '__main__':
         if j.rect.top < lim_superior:
             j.rect.top = lim_superior
             #j.velx=0
-            f_vy=15
+            f_vy=velocidad
         elif j.rect.bottom > lim_inferior:
             j.rect.bottom = lim_inferior
             #j.velx=0
-            f_vy=-15
+            f_vy=-velocidad
         else:
             f_vy = 0
 
 
-        jugadores.update()
+        ''' for b in bloques:
+            if f_x > f_limite_x:
+                b.velx=f_vx
+            else:
+                b.velx=0
+            
+            if f_y > f_limite_y:
+                b.vely=f_vy
+            else:
+                b.vely=0 '''
+        
+        for b in bloques:
+            b.velx=f_vx
+            b.vely=f_vy
+            
+
+        
+        
         
 
         # Refresco de pantalla
@@ -197,14 +417,20 @@ if __name__ == '__main__':
         #pantalla.fill(NEGRO)
         pantalla.blit(fondo, [f_x, f_y])
 
-        pantalla.blit(conejo[0][0], [100, 100])
+        #pantalla.blit(conejo[0][0], [100, 100])
+        
         pygame.display.flip()
+        
 
         # Dibujo de los elementos
         jugadores.draw(pantalla)
-        
+        bloques.draw(pantalla)
+
+        bloques.update()
+        jugadores.update()
         pygame.display.flip()
-        reloj.tick(10)
+        reloj.tick(5)
+        
 
         #Control del limite en x del fondo
         if f_x >= f_limite_x:
